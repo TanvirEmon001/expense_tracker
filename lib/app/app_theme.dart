1e8d0e28b940c6bfe0434494cfadaf3e5e40e50a
tanviremon001 <tanvirofficial121@gmail.com>
2026-03-04 23:22:43 +0600

    Home screen design in progress

--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -4,7 +4,13 @@ class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       brightness: Brightness.light,
-      scaffoldBackgroundColor: Color(0xFFDCDCDC),
+      scaffoldBackgroundColor: Color(0xFFEDEAEA),
+      appBarTheme: AppBarThemeData(
+        backgroundColor: Colors.white70,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.white70,
+      )
     );
   }
 
